58196b352be7481b8aa6ed465175729de4b6e20e
TICK TOCK <tick@tick.com>
2018-10-20 15:43:23 +0000

    influx

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,5 +1,20 @@
 class profile::base {
-
   #the base profile should include component modules that will be on all nodes
+  class { 'apt':
+    always_apt_update => true,
+  }
 
+  apt::key { 'influxdb.key':
+    source => 'https://repos.influxdata.com/influxdb.key',
+  } ->
+  apt::source { 'influxdb':
+    architecture => 'amd64',
+    location     => 'https://repos.influxdata.com/ubuntu',
+    repos        => 'stable',
+    release      => $::lsbdistcodename,
+  } ->
+  package { 'influxdb':
+    ensure  => 'latest',
+    require => Exec['apt_update'],
+  }
 }
