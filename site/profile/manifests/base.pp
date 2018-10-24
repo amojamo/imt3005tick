98e2a00b2cab12d086f27866e4b13a1244d664b1
TICK TOCK <tick@tick.com>
2018-10-24 07:32:33 +0000

    profile/role

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -15,4 +15,14 @@ class profile::base {
   package { ['influxdb','telegraf','kapacitor','chronograf']:
     ensure  => latest,
   }
+  
+  # exec { 'Watif for InfluxDB':
+  #  command => '/bin/sleep 8',
+  #
+  #}
+  #exec { 'Create admin user in InfluxDB':
+  #  command => "/usr/bin/influx -execute \"CREATE USER '$admin_usr' WITH PASSWORD '$admin_pwd' WITH ALL PRIVILEGES\"",
+  #  require => Exec['Wait for InfluxDB'],
+  #}
+
 }
