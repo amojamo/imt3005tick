ec79d4958d1745645a96eaf20156d56310357ec8
TICK TOCK <tick@tick.com>
2018-10-26 11:07:22 +0000

    Edit common and base

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,13 +1,13 @@
 class profile::base {
   #the base profile should include component modules that will be on all nodes
 
-  $operatingsystem = 'downcase($::facts['os']['name'])'
-  $oscodename = 'downcase($::facts['os']['distro']['codename'])'
+  $_operatingsystem = downcase($::facts['os']['name'])
+  $_oscodename = downcase($::facts['os']['distro']['codename'])
 
   apt::source { 'influxdb':
-    location     => "https://repos.influxdata.com/$operatingsystem",
+    location     => "https://repos.influxdata.com/${_operatingsystem}",
     repos        => 'stable',
-    release      => $oscodename,
+    release      => $_oscodename,
 		key 				 => {
 	    'id' 			 => '05CE15085FC09D18E99EFB22684A14CF2582E0C5',
 			'source'   => 'https://repos.influxdata.com/influxdb.key',
