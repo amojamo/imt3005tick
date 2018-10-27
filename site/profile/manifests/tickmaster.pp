b4d9029b45f1dd200ec607b7aa44776fa2a6c622
Askil <askilao@stud.ntnu.no>
2018-10-27 17:09:11 +0200

    Variable declaration

--- site/profile/manifests/tickmaster.pp
+++ site/profile/manifests/tickmaster.pp
@@ -1,7 +1,7 @@
 
 class profile::tickmaster (
-  String $admin_usr,
-  String $admin_pwd,
+  $admin_usr = lookup('infuxdb::admin_usr')
+  $admin_pwd = lookup('influxdb::admin_pwd')
 ) {
 
     package { ['influxdb','telegraf','kapacitor','chronograf']:
