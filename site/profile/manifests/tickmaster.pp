163e23d7de972ace0f31ac79aa23dc5f6de0efd2
Askil <askilao@stud.ntnu.no>
2018-10-28 14:32:20 +0100

    Syntax

--- site/profile/manifests/tickmaster.pp
+++ site/profile/manifests/tickmaster.pp
@@ -15,9 +15,11 @@ class profile::tickmaster {
   }
   exec { 'Create admin user in InfluxDB':
     command => "/usr/bin/influx -execute \"CREATE USER \"${admin_usr}\" WITH PASSWORD \'${admin_pwd}\' WITH ALL PRIVILEGES\"",
-    require => Exec['Wait for InfluxDB'],
-    require => Package['influxdb'],
-    logoutput => on_failure,
+    require => [
+      Exec['Wait for InfluxDB'],
+      Package['influxdb'],
+    ],
+    }
   }
   
   #Syntax from https://github.com/puppetlabs/puppetlabs-inifile
