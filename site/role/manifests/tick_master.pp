2ce35043df5fecbacdd8d3c8a24e3d1f0edeef96
Askil <askilao@stud.ntnu.no>
2018-11-01 10:13:10 +0100

    Added tickmaster to tickmaster role

--- site/role/manifests/tick_master.pp
+++ site/role/manifests/tick_master.pp
@@ -3,5 +3,5 @@ class role::tick_master {
   #This role would be made of all the profiles that need to be included to make a database server work
   #All roles should include the base profile
   include profile::base
-
+  include profile::tickmaster
 }
