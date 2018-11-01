ab8f457a5a9e37e22f3a5f179dfe6986d3375685
Askil <askilao@stud.ntnu.no>
2018-11-01 08:39:47 +0100

    Install HTTPD and test for HTTPD

--- spec/acceptance/installhttp.rb
+++ spec/acceptance/installhttp.rb
@@ -1,1 +1,19 @@
 
+test_name "install and running HTTPD" do
+
+	#Not install script on following platforms
+	confine :except, :platform => 'windows'
+
+	step "Install HTTPD" do
+		hosts.each do |hosts|
+			install_package(host, 'httpd') unless check_for_package(host, 'httpd')	
+		end
+	end
+
+	step 'start HTTPD' do 
+		hosts.each do |host|
+			on(host, "service httpd restart")
+		end
+	end
+
+end
