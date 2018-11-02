09296d0e32d6374292f153be306d63076f77ab54
Askil <askilao@stud.ntnu.no>
2018-11-02 18:47:20 +0100

    test

--- site/profile/manifests/telegraf.pp
+++ site/profile/manifests/telegraf.pp
@@ -3,7 +3,7 @@ class profile::telegraf {
 
     $admin_usr = lookup('influxdb::admin_usr')
     $admin_pwd = lookup('influxdb::admin_pwd')
-    $managerip = dns_a("manager.star.wars"):
+    $managerip = dns_a("manager.star.wars")
     notify { "Managerip:" ${managerip} } 
 
     package { ['influxdb','telegraf','kapacitor','chronograf']:
