f8389486a1cf8ec9e09e73cee990f68ac8fbe6e1
Askil <askilao@stud.ntnu.no>
2018-11-04 10:38:24 +0100

    Merge with testing

--- site/profile/manifests/telegraf.pp
+++ site/profile/manifests/telegraf.pp
@@ -3,9 +3,7 @@ class profile::telegraf {
 
     $admin_usr = lookup('influxdb::admin_usr')
     $admin_pwd = lookup('influxdb::admin_pwd')
-    $managerip = dns_a('manager.star.wars')[0]
-    
-    #notice("managerip ${managerip}")
+ 
     package { ['influxdb','telegraf','kapacitor','chronograf']:
     ensure => latest,
     notify => Service['influxdb'],
@@ -26,6 +24,7 @@ class profile::telegraf {
     'outputs.influxdb'  => {           #section of config file
       'username'        => "\"${admin_usr}\"", #setting in config file
       'password'        => "\"${admin_pwd}\"",   #setting in config file
+      'urls'            => "[\"http://manager.star.wars:8086\"]"
     }
   }
   create_ini_settings($userpw_telegraf, $defaults_telegraf)
@@ -34,6 +33,5 @@ class profile::telegraf {
     ensure  => running,
     enable  => true,
     require => Package['influxdb'],
-    before  => Exec['Create admin user in InfluxDB'],
     }
   }
