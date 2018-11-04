742dc5d6333524107d6d4cd006a9c81ebc63d66a
Askil <askilao@stud.ntnu.no>
2018-11-04 10:35:00 +0100

    Added ordering of classes in role

--- site/role/manifests/tick_master.pp
+++ site/role/manifests/tick_master.pp
@@ -3,7 +3,7 @@ class role::tick_master {
 
   #This role would be made of all the profiles that need to be included to make a database server work
   #All roles should include the base profile
-  include profile::base
+  include profile::base ->
   include profile::tickmaster
   #include profile::dns::server
 }
